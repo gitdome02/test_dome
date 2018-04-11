bcd2fb3df9961fa1259e9d6da116a2ad43481c67
zhanghai1989 <18736583606@163.com>
2018-04-11 15:32:25 +0800

    log error3

--- app/src/main/java/chy/exercise/com/jdshopping/view/MainActivity.java
+++ app/src/main/java/chy/exercise/com/jdshopping/view/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.e("message","zhanghai1989");
-        Log.e("msg","changhaiyang");
+        Log.e("msg","changhaiyang01");
     }
 }

93b2314829d6371d87294550edd34acbc216c1ac
crowder <chy_crowder@163.com>
2018-04-11 15:37:37 +0800

    log.erorr5

--- app/src/main/java/chy/exercise/com/jdshopping/view/MainActivity.java
+++ app/src/main/java/chy/exercise/com/jdshopping/view/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.e("message","zhanghai1989");
         Log.e("=====",">");
+        Log.e("<","=======");
     }
 }
